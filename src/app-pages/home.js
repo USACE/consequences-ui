44086f2161fcebdef93b891db4794335b3baaa31
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-10-07 14:24:18 -0500

    add depth events

--- src/app-pages/home.js
+++ src/app-pages/home.js
@@ -1,23 +1,44 @@
 import React from "react";
+import { connect } from "redux-bundler-react";
 import Navbar from "../app-components/navbar";
+
+const EventList = connect(
+  "selectEventItemsArray",
+  ({ eventItemsArray: events }) =>
+    events &&
+    events.length && (
+      <div class="list-group">
+        {events.map((v, idx) => (
+          <span key={idx}
+            onClick={() => {
+              console.log(`Clicked: ${v.id}`)
+            }}
+            class="list-group-item list-group-item-action flex-column align-items-start"
+          >
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">{v.name}</h5>
+                <small>Created<strong className="px-3">{v.create_date}</strong>by user<strong className="px-3">{v.creator}</strong></small>
+            </div>
+          <small>UUID: {v.id}</small>
+          </span>
+        ))}
+      </div>
+    )
+);
+
 export default () => (
   <>
-  <Navbar />
-    <main role="main" class="container">
-      <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p class="lead">
-          This example is a quick exercise to illustrate how the top-aligned
-          navbar works. As you scroll, this navbar remains in its original
-          position and moves with the rest of the page.
-        </p>
-        <a
-          class="btn btn-lg btn-primary"
-          href="/docs/4.3/components/navbar/"
-          role="button"
-        >
-          View navbar docs &raquo;
-        </a>
+    <Navbar />
+    <main role="main" className="container">
+      <div>
+      <h3 className="mb-4">Damage Computes</h3>
+        <div className="mt-4 mb-5">
+          --- List Here ---
+        </div>
+        <h3 className="mb-4">Depth Events</h3>
+        <div className="mt-4">
+          <EventList />
+        </div>
       </div>
     </main>
   </>
